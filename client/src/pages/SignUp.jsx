b2739458b6a723313ba462423c67cfcc0e42071f
Ahmed Hesham <ahmedh246891@gmail.com>
2024-01-21 02:39:18 +0200

    complete sign up page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,38 +1,74 @@
 import { Link } from "react-router-dom";
 import Button from "../components/Button";
+import { useState } from "react";
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(false);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      setLoading(false);
+      if (data.success === false) {
+        setError(true);
+        return;
+      }
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
+
   return (
     <div className="mx-auto p-3 max-w-lg">
       <h1 className="font-semibold text-3xl text-center my-7">Sign Up</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="text"
           placeholder="Username"
           id="username"
           className="bg-slate-100 rounded-lg p-3"
+          onChange={handleChange}
         />
         <input
-          type="mail"
+          type="email"
           placeholder="Email"
           id="email"
           className="bg-slate-100 rounded-lg p-3"
+          onChange={handleChange}
         />
         <input
           type="password"
           placeholder="Password"
           id="password"
           className="bg-slate-100 rounded-lg p-3"
+          onChange={handleChange}
         />
-        <Button bgColor="bg-slate-700">Sign up</Button>
-        <Button bgColor="bg-red-700">Continue With Google</Button>
+        <Button disabled={loading} bgColor="bg-slate-700" type="submit">
+          {loading ? "Loading..." : "Sign up"}
+        </Button>
+        {/* <Button bgColor="bg-red-700">Continue With Google</Button> */}
       </form>
       <div className="flex gap-2 mt-5">
-        <p className="font-bold">Have an account?</p>
+        <p>Have an account?</p>
         <Link to="/sign-in">
           <span className="text-blue-500">Sign In</span>
         </Link>
       </div>
+      {error && <p className="text-red-700 mt-5">Something went wrong!</p>}
     </div>
   );
 };
